9c2c63762b1b5d7b2d2c814ff9abb552af18ca97
Grigorij_Kosov <kosov.gr@mail.ru>
2021-04-14 00:33:18 +0500

    Second try

--- Essen jagen/Assets/CharacterA.cs
+++ Essen jagen/Assets/CharacterA.cs
@@ -1,5 +1,6 @@
-using System.Collections; 
-using System.Collections.Generic; 
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterA : MonoBehaviour
@@ -9,53 +10,55 @@ public class CharacterA : MonoBehaviour
     private Rigidbody2D rb2d;
     private SpriteRenderer sp;
     public float jumpForce;
-    private bool isGroubded;
+    private bool isGrounded;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    bool canDoubleJump = false;
+
+    private bool canDoubleJump;
     
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        sp = GetComponent<SpriteRenderer>();
+
+        sp = GetComponent<SpriteRenderer>(); 
     }
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (isGroubded)
+            if (isGrounded)
             {
                 Jump();
                 canDoubleJump = true;
             }
+            
             else if (canDoubleJump)
             {
                 Jump();
-                canDoubleJump = false;
             }
         }
     }
 
     private void FixedUpdate()
     {
-        xInput = Input.GetKey(KeyCode.RightArrow) 
-            ? Input.GetAxis("Horizontal") 
-            : Input.GetKey(KeyCode.LeftArrow) 
-                ? Input.GetAxis("Horizontal") : 0;
-        yInput = Input.GetKey(KeyCode.UpArrow) 
-            ? Input.GetAxis("Vertical") 
-            : Input.GetKey(KeyCode.DownArrow) 
-                ? Input.GetAxis("Vertical") : 0;
-        transform.Translate(xInput*moveSpeed, yInput*moveSpeed,0f);
+        xInput = Input.GetKey(KeyCode.LeftArrow) ? Input.GetAxis("Horizontal") :
+            Input.GetKey(KeyCode.RightArrow) ? Input.GetAxis("Horizontal") : 0;
+        //yInput = Input.GetKey(KeyCode.UpArrow) ? Input.GetAxis("Vertical") :
+        //    Input.GetKey(KeyCode.DownArrow) ? Input.GetAxis("Vertical") : 0;
+        transform.Translate(xInput * moveSpeed, yInput * moveSpeed, 0);
+        
         PlatformerMove();
         FlipPlayer();
 
-        isGroubded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
     void PlatformerMove()
@@ -63,21 +66,21 @@ public class CharacterA : MonoBehaviour
         rb2d.velocity = new Vector2(moveSpeed * xInput, rb2d.velocity.y);
     }
 
-    void Jump()
-    {
-        rb2d.velocity = Vector2.up * jumpForce;
-    }
     void FlipPlayer()
     {
-        if (rb2d.velocity.x < -0f)
+        if (rb2d.velocity.x < 0f)
         {
             sp.flipX = true;
         }
-        
-        else if (rb2d.velocity.x > 0f)
+
+        else if(rb2d.velocity.x > 0f)
         {
             sp.flipX = false;
         }
     }
-}
 
+    void Jump()
+    {
+        rb2d.velocity = Vector2.up * jumpForce;
+    }
+}
